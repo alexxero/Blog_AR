d58eb17f2c14952870d644d266580f0ea7689883
alexxero <alexxero@mail.ru>
2016-05-05 20:18:43 +0300

    NewPost page added

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:Blog.db"
+
+class Post < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb "Hello!"
+end
+
+get '/NewPost' do
+  erb :NewPost
 end
